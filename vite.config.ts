8d6608738107ec0bdfacebb1f0c5b7cdde2e2db6
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-02-26 15:04:58 -0300

    fix: fixing absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
+  resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
   plugins: [react()],
